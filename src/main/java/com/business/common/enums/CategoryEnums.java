a2fbea19eb10c7498ae632b062beaf5317176efb
Yanxiaofei0916 <591482927@qq.com>
2024-03-05 10:42:37 +0800

    初始化

---
+++ src/main/java/com/business/common/enums/CategoryEnums.java
@@ -0,0 +1,23 @@
+package com.business.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 游戏分类枚举类
+ * @Author: yxf
+ */
+@AllArgsConstructor
+@Getter
+public enum CategoryEnums {
+
+    HOT(1, "热门推荐"),
+
+    EVERYONE_IS_PLAYING(2, "大家都在玩"),
+
+    BRAZIL_ELECTRON(3,"巴西电子");
+
+    private final Integer code;
+    private final String name;
+
+}
